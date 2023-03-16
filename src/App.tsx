df60d0971b4c5b5d08244ac4e41fdf2f5180fd92
Justas-Sind <justas.sindar@gmail.com>
2023-03-16 18:26:03 +0200

    Installed dev dependencies, routes set-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css'
+import "./App.css";
+import {Routes, Route, Navigate } from "react-router-dom"
 
 function App() {
-
   return (
-    <div className="App">
-
-    </div>
-  )
+    <Routes>
+      <Route path="/" element={<h1>Hi</h1>}></Route>
+      <Route path="/new" element={<h1>New</h1>}></Route>
+      <Route path="/:id">
+        <Route index element={<h1>Show</h1>} />
+        <Route path="edit" element={<h1>Edit</h1>} />
+      </Route>
+      <Route path="/*" element={<Navigate to="/" />}></Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
