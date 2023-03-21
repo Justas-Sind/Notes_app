93879732d2dc001ba30cdc6bd74fe472fbcf92b9
Justas-Sind <justas.sindar@gmail.com>
2023-03-21 19:19:29 +0200

    All functionalities implemented

--- src/NoteList.tsx
+++ src/NoteList.tsx
@@ -1,7 +1,8 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, Fragment } from "react";
 import { Link } from "react-router-dom";
 import ReactSelect from "react-select";
 import { Tag } from "./App";
+import { Dialog, Transition } from "@headlessui/react";
 
 type SimplifiedNote = {
   tags: Tag[];
@@ -12,11 +13,31 @@ type SimplifiedNote = {
 type NoteListProps = {
   availableTags: Tag[];
   notes: SimplifiedNote[];
+  deleteTag: (id: string) => void
+  updateTag: (id: string, label: string) => void
 };
 
-function NoteList({ availableTags, notes }: NoteListProps) {
+type ModalProps = {
+  modalIsOpen: boolean;
+  closeModal: () => void;
+  availableTags: Tag[]
+  deleteTag: (id: string) => void
+  updateTag: (id: string, label: string) => void
+};
+
+function NoteList({ availableTags, notes, deleteTag, updateTag }: NoteListProps) {
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
   const [title, setTitle] = useState("");
+
+  let [modalIsOpen, setModalIsOpen] = useState(false);
+
+  function closeModal() {
+    setModalIsOpen(false);
+  }
+
+  function openModal() {
+    setModalIsOpen(true);
+  }
 
   const filteredNotes = useMemo(() => {
     return notes.filter((note) => {
@@ -41,7 +62,7 @@ function NoteList({ availableTags, notes }: NoteListProps) {
           <Link to="/new">
             <button>Create</button>
           </Link>
-          <button>Edit Tags</button>
+          <button onClick={() => openModal()}>Edit Tags</button>
         </div>
       </div>
       <form>
@@ -89,6 +110,7 @@ function NoteList({ availableTags, notes }: NoteListProps) {
           />
         ))}
       </div>
+      <EditTagsModal modalIsOpen={modalIsOpen} closeModal={closeModal} availableTags={availableTags} deleteTag={deleteTag} updateTag={updateTag} />
     </>
   );
 }
@@ -112,6 +134,68 @@ function NoteCard({ id, title, tags }: SimplifiedNote) {
         </div>
       </Link>
     </div>
+  );
+}
+
+function EditTagsModal({ modalIsOpen, closeModal, availableTags, deleteTag, updateTag }: ModalProps) {
+  return (
+    <Transition show={modalIsOpen} as={Fragment}>
+      <Dialog className="relative z-10" onClose={() => closeModal()}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-black bg-opacity-25" />
+        </Transition.Child>
+        <div className="fixed inset-0 flex items-center justify-center p-4">
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+              <Dialog.Title
+                as="h3"
+                className="text-lg font-medium leading-6 text-gray-900"
+              >
+                Edit Tags
+              </Dialog.Title>
+
+              <form>
+                <div>
+                  {availableTags.map(tag => (
+                    <div key={tag.id}>
+                      <div>
+                        <input type="text" value={tag.label} onChange={e => updateTag(tag.id, e.target.value)} />
+                      </div>
+                      <div>
+                        <button onClick={() => deleteTag(tag.id)}>&times;</button>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+              </form>
+
+              <button
+                onClick={() => closeModal()}
+                className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+              >
+                Close
+              </button>
+            </Dialog.Panel>
+          </Transition.Child>
+        </div>
+      </Dialog>
+    </Transition>
   );
 }
 
